d1a7bf7dd2961bcbe421c1d1e3ea9d309237730d
kingranking <switchmario@i.softbank.jp>
2020-07-05 18:13:17 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.14.1'
 set :application, 'chat-space2'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:kingranking/chat-sapce2.git'
+set :repo_url,  'git@github.com:kingranking/chat-space2.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
